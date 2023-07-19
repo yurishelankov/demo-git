be4db0ad59293e2b8456b1d3cb335483ef9c5b07
cheldon777 <yshelankov@gmail.com>
2023-07-20 00:21:42 +0300

    refactor world-java

---
+++ Guess.java
@@ -0,0 +1,13 @@
+public class Guess {
+
+    public static void main(String[] args)
+            throws java.io.IOException {
+        char ch, answer = 'K';
+        System.out.println("задумана буква  в диапазоне A-Z.");
+        System.out.print("попытайтесь ее угадать:");
+        ch = (char) System.in.read();
+        if (ch == answer) System.out.println("правильно!");
+
+    }
+
+}
