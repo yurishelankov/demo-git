86635ff9df918b1728ac8a73b531f8d18d833300
cheldon777 <yshelankov@gmail.com>
2023-07-20 23:46:40 +0300

    add - add class While

---
+++ While.java
@@ -0,0 +1,10 @@
+public class While {
+    public static void main(String[] args) {
+        char ch;
+        ch = 'a';
+        while (ch <= 'z'){
+            System.out.print(ch);
+            ch++;
+        }
+    }
+}
