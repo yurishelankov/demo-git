db453c432ecb52061204b1b97721de83359919ed
cheldon777 <yshelankov@gmail.com>
2023-07-20 23:43:29 +0300

    add - add class ForVar

---
+++ ForVar.java
@@ -0,0 +1,10 @@
+public class ForVar {
+    public static void main(String[] args) {
+        int sum = 0;
+        int fact = 1;
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("сумма :" + sum);
+            System.out.println("факториал:" + fact);
+        }
+    }
+}
